4c16cebf84140058d13cc785cb0fa036e6560de0
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-06-15 14:42:44 -0500

    Add data fetch

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ window.addEventListener("load", function () {
       let isValid = inputValidation();
       if (isValid) {
          updateShuttle();
+         fetchData();
       }
    });
 });
@@ -84,4 +85,24 @@ function updateShuttle() {
       fuelStatus.innerHTML = "Fuel level high enough for launch";
       cargoStatus.innerHTML = "Cargo mass low enough for launch";
    }
+}
+
+function fetchData() {
+   let missionTarget = document.getElementById("missionTarget");
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+      response.json().then(function (json) {
+         console.log(json);
+         missionTarget.innerHTML =
+            `<h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[5].name}</li>
+               <li>Diameter: ${json[5].diameter}</li>
+               <li>Star: ${json[5].star}</li>
+               <li>Distance from Earth: ${json[5].distance}</li>
+               <li>Number of Moons: ${json[5].moons}</li>
+            </ol>
+            <img src="${json[5].image}">`;
+      });
+   });
 }
