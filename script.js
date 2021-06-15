1ebc52ec75e58e82143ac91465ac9bc44b80bdf6
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-06-15 11:49:40 -0500

    Add alert for missing inputs

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Write your JavaScript code here!
-
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
@@ -11,3 +9,30 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function () {
+
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function (event) {
+
+      // Input validation
+      let userInputs = document.querySelectorAll("input");
+      let missingInput = false;
+
+      // Check each input for missing value
+      userInputs.forEach(function (input) {
+         if (input.value === "") {
+            missingInput = true;
+         }
+      });
+
+      // Alert if missing input
+      if (missingInput) {
+         alert("All fields are required.");
+      }
+
+      // Don't submit form yet
+      event.preventDefault();
+   });
+});
+
